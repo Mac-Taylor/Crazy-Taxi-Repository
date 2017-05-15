3185e42f603c63ae5377063784a9a83a137d9ec8
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-15 15:40:25 -0400

    3:40

--- js/views/play.js
+++ js/views/play.js
@@ -4,6 +4,12 @@ let View = require('ampersand-view');
 module.exports = View.extend({
     template: document.querySelector('#grid-template').innerHTML,
 
+    bindings: {
+        'model.taxiX': '',
+        'model.taxiY': '',
+        'model.gas': ''
+    },
+
     events: {
         'click .leftbutton': 'moveLeft',
         'click .righttbutton': 'moveRight',
